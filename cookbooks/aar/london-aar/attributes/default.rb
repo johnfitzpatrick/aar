ebf20588ad4dbe59c5cf2aea349ae98cb77c1027
johnfitzpatrick <jhnftzptrck@gmail.com>
2014-10-17 12:34:28 +0100

    removed some old files

--- cookbooks/aar/london-aar/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-
-default["root_dbpswd"] = "dbpassword123"
-# default["prompt"] = "enter the mysql root user password: "
-default["logdir"] = "/var/log"
-default["secretkey"] = "abcdefghijkl"
-default["password"] = "123456"
