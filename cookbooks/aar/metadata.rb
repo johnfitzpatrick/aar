f3022688875803bb629f89acd0cacfd0a19015a8
johnfitzpatrick <jhnftzptrck@gmail.com>
2014-10-17 12:21:15 +0100

    reorganised files

---
+++ cookbooks/aar/metadata.rb
@@ -0,0 +1,8 @@
+name             'london-aar'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures london-aar'
+long_description 'Installs/Configures london-aar'
+version          '0.1.0'
+
